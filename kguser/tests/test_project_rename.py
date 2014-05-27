c569048c284a4b490dd6cc3b215a46ddc274fe61
Russell Sim <russell.sim@gmail.com>
2014-05-28 09:47:55 +1000

    Fixed failing test due to broken fixture
    Change-Id: Iea15018dc972468db94b04acaf9cc9f0a30ecc97

--- kguser/tests/test_project_rename.py
+++ kguser/tests/test_project_rename.py
@@ -1,18 +1,12 @@
-
-# Setup the broken Karaage test suite path
-import sys
-from os import path
-
-from unit import TestCase
 from django.core.urlresolvers import reverse
-from factory.django import DjangoModelFactory
+from karaage.tests.unit import UnitTestCase
 from karaage.projects.models import Project
 
 from fixtures import (PersonFactory, AccountFactory,
                       ProjectQuotaFactory, ProjectRenamedFactory)
 
 
-class ProjectRenameTestCase(TestCase):
+class ProjectRenameTestCase(UnitTestCase):
 
     def test_project_renamed(self):
         """Create a new project that has been renamed correctly."""
@@ -48,10 +42,11 @@ class ProjectRenameTestCase(TestCase):
                          "Projects shouldn't be allowed to rename more than once.")
 
     def test_project_name_change(self):
-        account = AccountFactory()
+        account = AccountFactory(machine_category=self.machine_category)
         project = account.default_project
         person = account.person
-        ProjectQuotaFactory(project=project, machine_category=account.machine_category)
+        ProjectQuotaFactory(project=project,
+                            machine_category=self.machine_category)
 
         # Set the project app approve by to the tom account
         approver = PersonFactory(username='tom-karaage')
@@ -81,14 +76,20 @@ class ProjectRenameTestCase(TestCase):
 
         # After changing a project name the user should be redirected to
         # the new project page.
+        new_pid = 'my_project'
+        old_pid = project.pid
         response = self.client.post(reverse('confirm_project_name',
                                             kwargs={'project_id': project.pid}),
                                     {'name': 'My Project',
-                                     'pid': 'my_project'}, follow=True)
+                                     'pid': new_pid}, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.redirect_chain,
                          [('http://testserver/projects/my_project/', 302)])
+
         self.datastore.save_project.assert_called_with(project)
+        self.datastore.set_project_pid.assert_called_with(project, old_pid, new_pid)
+        self.datastore.set_group_name.assert_called_with(
+            Project.objects.get(pid=new_pid).group, old_pid, new_pid)
 
         project = Project.objects.get(id=project.id)
         self.assertEqual(project.name, 'My Project')
